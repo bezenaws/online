5d357a51c317341bf3e094a9fb2500b514fd89fa
bezenaws <bijendra0830@gmail.com>
2020-09-10 17:25:11 -0400

    jenkins test

--- app/footer.php
+++ app/footer.php
@@ -9,7 +9,7 @@
 				<div style="height: 60px;" class="hidden-print"></div>
 				<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
 					<p class="navbar-text"><small>
-						Powered by DevOps Tranning.
+						Test Jenkins.
 					</small></p>
 				</nav>
 			<?php } ?>
